680c512781e1d4cfd573ab059ce5990e0ef3ec14
Sumit Bhardwaj <40798467+nvkex@users.noreply.github.com>
2020-05-31 09:54:41 +0530

    implement canvasjs

--- rs-script.js
+++ rs-script.js
@@ -8,9 +8,11 @@ var currentStudentIndex = 0;
 var currentStudent = studentNameArray[currentStudentIndex];
 $(document).ready(function () {
 
-    $(".error-msg").hide();
+    // only login page visible initially
+    $("#error-msg").hide();
     $(".dashboard").hide();
 
+    // on successful login switch to dashboard
     $("#loginBtn").click(function () {
         if ($("#studentPass").val() == studentPassword[0]) {
             $(".login-container").fadeOut(200);
@@ -21,4 +23,37 @@ $(document).ready(function () {
             $('#error-msg').fadeIn();
         }
     });
+
+    $('#submitData').click(() => {
+        // switch from dashboard to graph canvas
+        $(".dashboard").fadeOut(200);
+        $('.result-container').fadeIn(3000);
+
+        // Get graphical representation of data from canvasjs
+        var chart = new CanvasJS.Chart("graphCanvas", {
+            animationEnabled: true,
+            theme: "dark2",
+            title: {
+                text: "Student Report"
+            },
+
+            axisY: {
+                title: "Marks"
+            },
+            data: [{
+                type: "column",
+                dataPoints: [
+                    { y: 60, label: "DM" },
+                    { y: 30, label: "DBMS" },
+                    { y: 75, label: "AWT" },
+                    { y: 21, label: "JAVA" },
+                    { y: 87, label: "OS" }
+                ]
+            }]
+        });
+        chart.render();
+
+    });
+
+
 });
